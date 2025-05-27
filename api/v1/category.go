33f2c9bd69506795ee79af0f5a202e6201fe1756
lucky798213 <3424606473@qq.com>
2025-05-27 17:32:14 +0800

    博客分类接口编写

--- api/v1/category.go
+++ api/v1/category.go
@@ -1,13 +1,77 @@
 package v1
 
-//查询分类名是否存在
+import (
+	"github.com/gin-gonic/gin"
+	"myblog/model"
+	"myblog/utils/errmsg"
+	"net/http"
+	"strconv"
+)
 
-//添加分类
+// 添加分类
+func AddCategory(c *gin.Context) {
+	var data model.Category
+	_ = c.ShouldBindJSON(&data)
+	code = model.CheckCategory(data.Name)
+	if code == errmsg.SUCCSE {
+		model.CreateCate(&data)
+	}
+	if code == errmsg.Error_CATENAME_USED {
+		code = errmsg.Error_CATENAME_USED
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
 
-//查询单个分类下的文章
+// 查询分类列表
+func GetCate(c *gin.Context) {
+	pageSize, err := strconv.Atoi(c.Query("pageSize"))
+	if err != nil || pageSize <= 0 {
+		pageSize = 10 // 默认每页10条
+	}
+	pageNum, err := strconv.Atoi(c.Query("pageNum"))
+	if err != nil || pageNum <= 0 {
+		pageNum = 1 // 默认第1页
+	}
 
-//查询分类列表
+	data := model.GetCate(pageSize, pageNum)
+	code = errmsg.SUCCSE
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
 
-//编辑分类
+// 编辑分类名
+func EditCate(c *gin.Context) {
+	var data model.Category
+	id, _ := strconv.Atoi(c.Param("id"))
+	_ = c.ShouldBindJSON(&data)
+	code = model.CheckCategory(data.Name)
+	if code == errmsg.SUCCSE {
+		model.EditCate(id, &data)
+	}
+	if code == errmsg.Error_CATENAME_USED {
+		c.Abort()
+	}
 
-//删除分类
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
+
+// 删除分类
+func DeleteCate(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	code = model.DeleteCate(id)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
--- model/Category.go
+++ model/Category.go
@@ -1,8 +1,62 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"myblog/utils/errmsg"
+)
 
 type Category struct {
-	gorm.Model
+	ID   uint   `gorm:"primary_key;auto_increment" json:"id"`
 	Name string `gorm:"type:varchar(20);not null " json:"name"`
+}
+
+// 查询分类是否存在
+func CheckCategory(name string) (code int) {
+	var cate Category
+	db.Select("id").Where("username = ?", name).First(&cate)
+	if cate.ID > 0 {
+		return errmsg.Error_CATENAME_USED //1001
+	}
+	return errmsg.SUCCSE
+}
+
+// 新增分类
+func CreateCate(data *Category) int {
+	err := db.Create(&data).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCSE
+}
+
+// 查询用户列表
+func GetCate(pageSize int, pageNum int) []Category {
+	var cate []Category
+	err = db.Limit(pageSize).Offset((pageNum - 1) * pageSize).Find(&cate).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil
+	}
+	return cate
+}
+
+// 编辑分类信息
+func EditCate(id int, data *Category) int {
+	var cate Category
+	var maps = make(map[string]interface{})
+	maps["name"] = data.Name
+	err = db.Model(&cate).Where("id = ?", id).Updates(maps).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCSE
+}
+
+// 删除分类
+func DeleteCate(id int) int {
+	var cate Category
+	err = db.Where("id = ?", id).Delete(&cate).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCSE
 }
